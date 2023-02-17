1f426abdc061b06793900333dd71621f0e412586
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 10:15:33 -0500

    Create index.js

---
+++ Node Pt 2/NPM Intro/Language Detector/index.js
@@ -0,0 +1,14 @@
+import { franc } from "franc";
+import langs from 'langs';
+import colors from 'colors';
+
+const input = process.argv[2];
+console.log(input);
+const langCode = franc(input);
+console.log(langCode);
+if (langCode === 'und') {
+    console.log("SORRY, COULDN'T FIGURE IT OUT!  TRY AGAIN WITH MORE SAMPLE TEXT!".red)
+} else {
+    const language = langs.where("3", langCode);
+    console.log(`Our best guess is: ${language.name}`.green)
+}
