9333a000cf25e4dd1f53c11297ba6bd024ec7150
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 10:00:26 -0500

    Add files via upload

---
+++ Node Pt 1/Process vs Argv/greeter.js
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2);
+for (let arg of args) {
+    console.log(`Hello, ${arg}`)
+}
+
+// node boilerplate.js Project
+
+// Project
+//     index.html
+//     app.css
+//     app.js
+    
+    // All file system operations have synchronous and asynchronous forms.
+// Synchronous version means "do this thing and wait until it finishes to move on"
+// "Stops any other code from running until it completes."
